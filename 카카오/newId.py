a1c345fe73513660cc387cf9a93e105b92282938
102sae <95170874+102sae@users.noreply.github.com>
2022-05-08 21:23:23 +0900

    220508

---
+++ 카카오/newId.py
@@ -0,0 +1,35 @@
+import re
+def solution(new_id):
+    #1단계
+    new_id = new_id.lower()
+    #2단계
+    new_id = re.sub('[^a-z0-9-_.]','',new_id)
+    #3단계
+    while ".." in new_id:
+        new_id = new_id.replace('..', '.')
+    #4단계
+    if(new_id[0]=='.'):
+        if(len(new_id)>1):
+            new_id = new_id[1:]
+    if(new_id[-1]=='.'):
+        new_id = new_id [:-1];
+    
+    #5단계   
+    if(new_id == ''):
+        new_id += 'a'
+    
+    #6단계
+    if(len(new_id)>15):
+        new_id = new_id[:15]
+        print(new_id)
+        if(new_id[-1]=='.'):
+            new_id = new_id[:-1]
+    
+    #7단계
+    if len(new_id)==1:
+        new_id += new_id
+    if len(new_id) == 2:
+        new_id += new_id[-1]
+        
+    
+    return new_id
